60d1062c53d6b5392b27f8f57e7f64f0dbbaada4
cunk111 <tom.modeste@gmail.com>
2019-03-05 14:38:16 +0100

    Fixing typo in user reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,14 +1,14 @@
 import initialState from './initialState'
 import { FETCH_USERS, RECEIVE_USERS } from '../actions/actionTypes'
 
-export default function stuff(state = initialState.stuff, action) {
+export default function users(state = initialState.users, action) {
   let newState
   switch (action.type) {
   case FETCH_USERS:
     console.log('FETCH_USERS Action')
     return action
   case RECEIVE_USERS:
-    newState = action.stuff
+    newState = action.users
     console.log('RECEIVE_USERS Action')
     return newState
   default:
